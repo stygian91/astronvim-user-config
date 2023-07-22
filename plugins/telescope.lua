6d290f57817d394373ebdb9c979caebdd535463d
Stygian <kioravuser@gmail.com>
2023-07-22 16:14:02 +0300

    Cleanup telescope config.

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,19 +1,3 @@
--- local telescope = require("telescope")
--- local lga_actions = require("telescope-live-grep-args.actions")
---
--- telescope.setup {
---   extensions = {
---     live_grep_args = {
---       auto_quoting = true,
---       mappings = {
---         i = {
---           ["<C-k>"] = lga_actions.quote_prompt()
---         }
---       },
---     },
---   }
--- }
-
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
@@ -21,24 +5,9 @@ return {
     "debugloop/telescope-undo.nvim",
     "nvim-telescope/telescope-live-grep-args.nvim",
   },
-  opts = function()
-    -- local lga_actions = require("telescope-live-grep-args.actions")
-    -- return {
-    --   extensions = {
-    --     live_grep_args = {
-    --       auto_quoting = true,
-    --       mappings = {
-    --         i = {
-    --           ["<C-K>"] = lga_actions.quote_prompt()
-    --         }
-    --       },
-    --     },
-    --   }
-    -- }
-  end,
   config = function()
-    require("telescope").load_extension("live_grep_args")
-    -- vim.api.nvim_set_keymap("n", "<leader>fg",  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-    -- keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+    local tele = require("telescope")
+    tele.load_extension("live_grep_args")
+    tele.load_extension("undo")
   end
 }
