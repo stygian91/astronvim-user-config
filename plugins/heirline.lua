81258233c859168935092bc0c68d53c68421ceca
stygian91 <kioravuser@gmail.com>
2023-07-25 22:20:40 +0300

    Customize status bar.

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -2,6 +2,20 @@ return {
   "rebelot/heirline.nvim",
   opts = function (_, opts)
     local status = require("astronvim.utils.status")
-    opts.statusline[1] = status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }
+
+    opts.statusline = {
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }),
+      status.component.git_branch(),
+      status.component.file_info { filetype = {}, filename = false, file_modified = false },
+      status.component.git_diff(),
+      status.component.nav({ scrollbar = false }),
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      status.component.fill(),
+      status.component.lsp(),
+      status.component.treesitter(),
+    }
   end
 }
